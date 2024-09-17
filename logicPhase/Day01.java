173739628e50fbbdb4204020993426508b6576fd
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-17 15:03:24 +0700

    add code about diff21

--- logicPhase/Day01.java
+++ logicPhase/Day01.java
@@ -17,18 +17,36 @@ package logicPhase;
 // }
 
 //nearHundred
+// public class Day01 {
+//   public static void main(String[] args) {
+//     Day01 obj = new Day01();
+//     System.out.println(obj.nearHundred(89));
+//   }
+//     public boolean nearHundred(int n) {
+//       if(Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10){
+//         return true;
+//       }
+//       else{
+//         return false;
+//       }
+//     }
+    
+// }
+
+//dif21
 public class Day01 {
   public static void main(String[] args) {
     Day01 obj = new Day01();
-    System.out.println(obj.nearHundred(89));
+    System.out.println(obj.diff21(89));
   }
-    public boolean nearHundred(int n) {
-      if(Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10){
-        return true;
-      }
-      else{
-        return false;
-      }
+  public int diff21(int n) {
+    if(n>21){
+      return Math.abs((21-n) *2);
     }
+    else{
+      return Math.abs(21-n);
+    }
+  }
+  
     
 }
