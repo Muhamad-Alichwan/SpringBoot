77000f0d1bea82e304c18c7028524ced86fda2cc
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-17 14:48:13 +0700

    Change code

---
+++ logicPhase/Day01.java
@@ -0,0 +1,32 @@
+package logicPhase;
+
+// Sum Double
+// public class Day01 {
+//   public static void main(String[] args) {
+//     Day01 obj = new Day01();
+//     System.out.println(obj.sumDouble(2, 3)); // Example usage
+//   }
+
+//   public int sumDouble(int a, int b) {
+//     if (a == b) {
+//       return (a + b) * 2;
+//     } else {
+//       return a + b;
+//     }
+//   }
+// }
+
+//nearHundred
+public class Day01 {
+  public static void main(String[] args) {
+    System.out.println(nearHundred(89));
+    public boolean nearHundred(int n) {
+      if(Math.abs(100 - n) <= 10 || Math.abs(200 - n) <= 10){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    
+}
