349633419922eab99655636126e13caa6f0ab33c
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-18 13:39:09 +0700

    move file to folder Day01

---
+++ logicPhase/Day01/sumDouble.java
@@ -0,0 +1,15 @@
+package logicPhase.Day01;
+
+public class sumDouble {
+  public static void main(String[] args) {
+    System.out.println(sumdouble(2, 3)); // Example usage
+  }
+  public static int sumdouble(int a, int b) {
+    if (a == b) {
+      return (a + b) * 2;
+    } else {
+      return a + b;
+    }
+  }
+
+}
