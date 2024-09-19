65973a331cd4c14aec5a01e233b917228b81279a
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-19 14:25:05 +0700

    modified function hitunghari, add function leapYear

--- logicPhase/Day03/Day03.java
+++ logicPhase/Day03/Day03.java
@@ -2,14 +2,23 @@ package logicPhase.Day03;
 import java.util.Scanner;
 /* Objective:
  * 1. dayName(int number). Fungsi ini menerima inputan angka dan mengembalikan hari apa sesuai urutan angka tersebut
+ * 
  * 2. dayNumber(String day). Fungsi ini menerima inputan nama hari dan mengembalikan angka hari ke berapa
  * Note: hari dimulai dari senin
- * 3. Dengan memanfaatkan 2 fungsi diatas, buatlah function, dengan  parameter day sebagai string, yang mencetak kata2 sebagai berikut:
+ * 
+ * 3. Dengan memanfaatkan 2 fungsi diatas, buatlah function, dengan  parameter day sebagai string, 
+ * yang mencetak kata2 sebagai berikut:
  *  Kemarin lusa adalah hari ....(2 hari sebelum parameter)
  *  kemarin adalah hari ....(1 hari sebelum parameter)
  *  hari ini adalah hari ....(didapat dari parameter)
  *  besok adalah hari ....(1 hari setelah parameter)
  *  besok lusa adalah hari ....(2 hari setelah parameter)
+ * 
+ * 4. Buatlah function leapYear() dengan parameter year yang berfungsi untuk mencetak tahun tahun kabisat 
+ *    mulai dari nilai yang diambil dari parameter, sampai tahun ini
+ * 
+ * 5. Buatlah function oddEvenAddition() dengan parameter number sebagai jumlah iterasi yang berfungsi
+ *    untuk melakukan penjumlahan bilangan ganjil gepan.
  * */
 public class Day03 {
 public static String dayName(int number) {
@@ -36,7 +45,7 @@ public static String dayName(int number) {
         case 6:
             hari = "Sabtu";
             break;
-        case 0:
+        case 7:
             hari = "Minggu";
             break;
         default:
@@ -69,23 +78,40 @@ public static String dayName(int number) {
       case "minggu":
         number = 7;
         break;
- 
       default:
-        number = 0;
-        break;
+        throw new IllegalArgumentException("Hari tidak valid: " + day);
     }
     return number;
   }
 
   public static void hitunghari(String day) {
     int number = dayNumber(day);
-    System.out.println("Kemarin lusa adalah hari " + dayName((number - 2 + 7)%7));
-    System.out.println("Kemarin adalah hari " + dayName((number - 1 + 7)%7));
+    System.out.println("Kemarin lusa adalah hari " + dayName((number - 2 + 7-1)%7+1));
+    System.out.println("Kemarin adalah hari " + dayName((number - 1 + 7-1)%7+1));
     System.out.println("Hari ini adalah hari " + dayName(number));
-    System.out.println("Besok adalah hari " + dayName((number + 1)%7));
-    System.out.println("Besok lusa adalah hari " + dayName((number + 2)%7));
+    System.out.println("Besok adalah hari " + dayName((number + 1-1)%7+1));
+    System.out.println("Besok lusa adalah hari " + dayName((number + 2-1)%7+1));
   }
 
+  public static void leapYear(int year) {
+    if (year%400 == 0 || year%400 != 0 && year%100 != 0 && year%4 == 0 ){
+      for (int i = year; i <= 2024; i++) {
+        if(i%400 == 0 || i%400 != 0 && i%100 != 0 && i%4 == 0){
+          System.out.print(i + " ");
+        }
+      }
+    }
+    else{
+      System.out.println("Bukan tahun kabisat");
+    }  
+  }
+
+  public static void oddEvenAddition(int number){
+    for (int i = 0; i <= number; i++) {
+      
+    }
+  
+  }
   public static void main(String[] args) {
     // Scanner input = new Scanner(System.in);
     // System.out.println("Masukkan angka: ");
@@ -99,10 +125,16 @@ public static String dayName(int number) {
     // System.out.println(dayNumber(day));
     // input.close();
 
+    // Scanner input = new Scanner(System.in);
+    // System.out.println("Masukkan Hari: ");
+    // String day = input.nextLine();
+    // hitunghari(day);
+    // input.close();
+
     Scanner input = new Scanner(System.in);
-    System.out.println("Masukkan Hari: ");
-    String day = input.nextLine();
-    hitunghari(day);
+    System.out.println("Masukkan Tahun: ");
+    int year = input.nextInt();
+    leapYear(year);
     input.close();
   }
 }
