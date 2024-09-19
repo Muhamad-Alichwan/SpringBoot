00624cf9436a713c7afce73341dcd50d509b1f55
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-19 13:16:56 +0700

    modified code

--- logicPhase/Day03/Day03.java
+++ logicPhase/Day03/Day03.java
@@ -84,7 +84,6 @@ public static String dayName(int number) {
     System.out.println("Hari ini adalah hari " + dayName(number));
     System.out.println("Besok adalah hari " + dayName((number + 1)%7));
     System.out.println("Besok lusa adalah hari " + dayName((number + 2)%7));
-    
   }
 
   public static void main(String[] args) {
