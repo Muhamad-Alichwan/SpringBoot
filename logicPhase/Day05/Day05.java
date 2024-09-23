2ca69be639b10bb91999c263e0ad183334b44035
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-23 10:45:15 +0700

    Update Day05.java

--- logicPhase/Day05/Day05.java
+++ logicPhase/Day05/Day05.java
@@ -21,6 +21,12 @@ public class Day05 {
     kotak(5);
     System.out.println();
     diagonal(5);
+    System.out.println();
+    oddEven(5);
+    System.out.println();
+    angkaterbalik(5);
+    System.out.println();
+
   }
 
   public static void kotak(int size) {
@@ -68,6 +74,46 @@ public class Day05 {
     }
   }
 
+  public static void oddEven(int size) {
+    int arrA[][] = new int[size][size];
+    int ganjil = 1;
+    int genap = 2;
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        if (i%2 == 0) {
+          arrA[i][j] = ganjil ;
+          ganjil += 2;
+        } else {
+          arrA[i][j] = genap ;
+          genap += 2;
+        }
+      }
+    }
+
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+        System.out.print(arrA[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void angkaterbalik(int size) {
+    int arrA[][] = new int[size][size];
+    int num = size*size;
+    for (int i = 0; i < size; i++ ) {
+      for (int j = 0; j < size; j++ ) {
+        arrA[i][j] = num--;
+      }
+    }   
+    
+    for (int i = 0; i < arrA.length; i++) {
+      for (int j = 0; j < arrA.length; j++) {
+        System.out.print(arrA[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
 
 
 
