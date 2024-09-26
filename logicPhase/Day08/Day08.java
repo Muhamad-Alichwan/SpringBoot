bc457afdcb8ccd8617578ca22f15e76b18b73881
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-26 11:09:30 +0700

    update case 3 and case 4

--- logicPhase/Day08/Day08.java
+++ logicPhase/Day08/Day08.java
@@ -5,8 +5,8 @@ public class Day08 {
   public static void main(String[] args) {
     // tambah tambah = new tambah();
     // kurang kurang = new kurang();
-    // kali kali = new kali();
-    // bagi bagi = new bagi();
+    kali kali = new kali();
+    bagi bagi = new bagi();
     Scanner scanner = new Scanner(System.in);
     int input = -1;
     while (input != 0) {
@@ -18,8 +18,8 @@ public class Day08 {
       System.out.println("4. Bagi");
       System.out.print("Masukan Angka: ");
 
-      input = scanner.nextInt();
       if(input != 0 ){
+        input = scanner.nextInt();
         switch (input) {
           case 1:
             System.out.print("Masukan angka ke-1: ");
@@ -44,7 +44,9 @@ public class Day08 {
             double setAngka1 = scanner.nextDouble();
             System.out.print("Masukan angka ke-2: ");
             double setAngka2 = scanner.nextDouble();
-            kali kali = new kali(setAngka1,setAngka2);
+            // kali kali = new kali(setAngka1,setAngka2);
+            kali.setAngka1(setAngka1);
+            kali.setAngka2(setAngka2);
             System.out.printf("Hasil %.2f * %.2f = %.2f\n", setAngka1, setAngka2, kali.perhitunganKali());
             break;
 
@@ -53,10 +55,15 @@ public class Day08 {
             double setAngka1Bagi = scanner.nextDouble();
             System.out.print("Masukan angka ke-2: ");
             double setAngka2Bagi = scanner.nextDouble();
-            bagi bagi = new bagi(setAngka1Bagi,setAngka2Bagi);
+            // bagi bagi = new bagi(setAngka1Bagi,setAngka2Bagi);
+            bagi.setAngka1(setAngka1Bagi);
+            bagi.setAngka2(setAngka2Bagi);
             System.out.printf("Hasil %.2f / %.2f = %.2f\n", setAngka1Bagi, setAngka2Bagi, bagi.perhituganBagi());
             break;
-        
+          
+          case 0:
+            System.out.println("Keluar Dari Program Kalkulator !!!!");
+            
           default:
             break;
         }
