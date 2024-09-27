24216aaa8c6a16e8e5863f8da7b0f193c6d419d8
Muhamad Alichwan <alichwan.muhamad@gmail.com>
2024-09-27 09:30:00 +0700

    try to use private property

--- logicPhase/Day08/bangunDatar/bangunDatar.java
+++ logicPhase/Day08/bangunDatar/bangunDatar.java
@@ -4,5 +4,13 @@ public abstract class bangunDatar implements kelilingLuas{
   public double horizontal;
   public double vertikal;
   public double diagonal;
-  public double jariJari;
+  private double jariJari;
+  public double getJariJari() {
+    return jariJari;
+  }
+  public void setJariJari(double jariJari) {
+    this.jariJari = jariJari;
+  }
+
+  
 }
--- logicPhase/Day08/bangunDatar/lingkaran.java
+++ logicPhase/Day08/bangunDatar/lingkaran.java
@@ -4,17 +4,17 @@ public class lingkaran extends bangunDatar{
   public lingkaran(){
   }
   public lingkaran(double jariJari){
-    this.jariJari = jariJari;
+    setJariJari(jariJari);
   }
   @Override
   public void keliling() {
-    double hasilKeliling = 2 * 3.14 * jariJari;
+    double hasilKeliling = 2 * 3.14 * getJariJari();
     System.out.println("ini Keliling Lingkaran: ");
     System.out.println(hasilKeliling);
   }
   @Override
   public void luas() {
-    double hasilLuas = 3.14 * (jariJari*jariJari);
+    double hasilLuas = 3.14 * (getJariJari()*getJariJari());
     System.out.println("ini Luas Lingkaran: ");
     System.out.println(hasilLuas);
   }
